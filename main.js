779a296c5fa0623793e1b19328beefa1be31b537
Aloder2006 <82025827+Aloder2006@users.noreply.github.com>
2022-06-27 23:59:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+let section = document.querySelectorAll("section");
+
+section.forEach(()=>{
+  window.addEventListener("scroll",()=>{
+    window.scrollTo(0,0);
+  })
+})
+
+
+let darkMode = document.querySelector(".dark");
+let content = document.querySelector(".content");
+let name = document.querySelector(".information h2");
+let pContent = document.querySelector(".information p");
+let hippos = document.querySelector(".information .skills p");
+let shadowBox = document.querySelector(".information .skills");
+
+let openDark = false;
+
+darkMode.addEventListener("click",()=>{
+  if(!openDark){
+    darkMode.className = "fa-solid fa-sun dark";
+    content.style.backgroundColor = "rgba(3, 0, 41, .6)";
+    name.style.color = "#eee";
+    pContent.style.color = "#eee";
+    hippos.style.color = "#eee";
+    shadowBox.style.boxShadow = "0 0 8px #999";
+    openDark = true;
+  } else {
+    darkMode.className = "fa-solid fa-moon dark";
+    content.style.backgroundColor = "rgba(238, 238, 238,.7)";
+    name.style.color = "#000";
+    pContent.style.color = "#000";
+    hippos.style.color = "#000";
+    shadowBox.style.boxShadow = "0 0 8px #333";
+    openDark = false;
+  }
+})
